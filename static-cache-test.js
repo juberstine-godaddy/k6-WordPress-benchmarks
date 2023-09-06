4d3895d0293465057ebb916d408e44768e97c12c
Jeff Uberstine <juberstine@godaddy.com>
2023-09-05 20:18:16 -0700

    add project id

--- static-cache-test.js
+++ static-cache-test.js
@@ -17,21 +17,22 @@ export let options = {
   batch: 1,
   throw: true,
   stages: [
-    { duration: '15m', target: 1000 },
+    { duration: '60s', target: 1 },
   ],
   ext: {
     loadimpact: {
+      projectID: 3657251,//put your project ID for k6 here
       distribution: {
-        Virginia: { loadZone: 'amazon:us:ashburn', percent: 10 },
-        London: { loadZone: 'amazon:gb:london', percent: 10 },
-        Frankfurt: { loadZone: 'amazon:de:frankfurt', percent: 10 },
-        Oregon: { loadZone: 'amazon:us:portland', percent: 10 },
-        Ohio: { loadZone: 'amazon:us:columbus', percent: 10 },
-        Tokyo: { loadZone: 'amazon:jp:tokyo', percent: 10 },
-        Sydney: { loadZone: 'amazon:au:sydney', percent: 10 },
-        Mumbai: { loadZone: 'amazon:in:mumbai', percent: 10 },
-        Singapore: { loadZone: 'amazon:sg:singapore', percent: 10 },
-        Brazil: { loadZone: 'amazon:br:sao paulo', percent: 10 },
+        Virginia: { loadZone: 'amazon:us:ashburn', percent: 100 },
+        // London: { loadZone: 'amazon:gb:london', percent: 10 },
+        // Frankfurt: { loadZone: 'amazon:de:frankfurt', percent: 10 },
+        // Oregon: { loadZone: 'amazon:us:portland', percent: 10 },
+        // Ohio: { loadZone: 'amazon:us:columbus', percent: 10 },
+        // Tokyo: { loadZone: 'amazon:jp:tokyo', percent: 10 },
+        // Sydney: { loadZone: 'amazon:au:sydney', percent: 10 },
+        // Mumbai: { loadZone: 'amazon:in:mumbai', percent: 10 },
+        // Singapore: { loadZone: 'amazon:sg:singapore', percent: 10 },
+        // Brazil: { loadZone: 'amazon:br:sao paulo', percent: 10 },
       },
     },
   },
